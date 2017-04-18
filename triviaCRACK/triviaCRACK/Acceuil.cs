7c7e13d28fba7e35a889a62407ad3a41a31eaf26
leanice33 <leanice33@gmail.com>
2017-04-18 16:40:33 -0400

    Add files via upload

---
+++ triviaCRACK/triviaCRACK/Acceuil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triviaCRACK
+{
+    public partial class F_Acceuil : Form
+    {
+        public F_Acceuil()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MainForm form1 = new MainForm();
+            form1.Show();
+        }
+    }
+}
---
+++ triviaCRACK/triviaCRACK/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace triviaCRACK
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            DrawIt();
+        }
+
+        private void DrawIt()
+        {
+            System.Drawing.Graphics graphics = this.CreateGraphics();
+            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
+               50, 50, 150, 150);
+            graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
+            graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawIt();
+        }
+    }
+}
