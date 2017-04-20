b3cfb9e143d1b8fa1f22740eedc9836e173c3f2b
leanice33 <leanice33@gmail.com>
2017-04-20 15:55:28 -0400

    Add files via upload

--- triviaCRACK/triviaCRACK/MainForm.cs
+++ triviaCRACK/triviaCRACK/MainForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace triviaCRACK
 {
-    public partial class    MainForm : Form
+    public partial class MainForm : Form
     {
         string con = "Data Source=(DESCRIPTION="
         + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)"
@@ -21,22 +21,12 @@ namespace triviaCRACK
 
         OracleConnection objConn;
         public MainForm()
-        {
+        {          
             InitializeComponent();
             Connect();
             InitiatePlayers(variables.Players);
-            DrawIt();
-
         }
 
-        private void DrawIt()
-        {
-            System.Drawing.Graphics graphics = this.CreateGraphics();
-            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
-               50, 50, 150, 150);
-            graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
-            graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
-        }
 
         private void InitiatePlayers(int x)
         {
@@ -45,11 +35,13 @@ namespace triviaCRACK
             switch (x)
             {
                 case 2:
-                    for(int i = 0; i < 2; ++i)
+                    for (int i = 0; i < 2; ++i)
                     {
                         choose = new Username();
                         choose.ShowDialog();
                     }
+                    GB_P3.Visible = false;
+                    GB_P4.Visible = false;
                     break;
                 case 3:
                     for (int i = 0; i < 3; ++i)
@@ -57,6 +49,7 @@ namespace triviaCRACK
                         choose = new Username();
                         choose.ShowDialog();
                     }
+                    GB_P4.Visible = false;
                     break;
                 case 4:
                     for (int i = 0; i < 4; ++i)
@@ -69,7 +62,9 @@ namespace triviaCRACK
             //prepare orcl command
             OracleCommand oraAjout = new OracleCommand("JOUEUR", objConn);
             oraAjout.CommandText = "JOUEUR.initialize";
-            oraAjout.CommandType = CommandType.StoredProcedure;            //add player to database for each one            for (int i = 0; i < x; ++i)
+            oraAjout.CommandType = CommandType.StoredProcedure;
+            //add player to database for each one
+            for (int i = 0; i < x; ++i)
             {
                 OracleParameter orapamNum = new OracleParameter("pnom", OracleDbType.Varchar2);
                 orapamNum.Direction = ParameterDirection.Input;
@@ -77,6 +72,22 @@ namespace triviaCRACK
                 oraAjout.Parameters.Add(orapamNum);
                 oraAjout.ExecuteNonQuery();
                 oraAjout.Parameters.Clear();
+                //change groupboxes names to the actual player's name
+                switch (i)
+                {
+                    case 0:
+                        GB_P1.Text = variables.names[0];
+                        break;
+                    case 1:
+                        GB_P2.Text = variables.names[1];
+                        break;
+                    case 2:
+                        GB_P3.Text = variables.names[2];
+                        break;
+                    case 3:
+                        GB_P4.Text = variables.names[3];
+                        break;
+                }
             }
         }
         private void Connect()
@@ -86,9 +97,50 @@ namespace triviaCRACK
             MessageBox.Show(objConn.State.ToString());
 
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void HideAllCats()
         {
-            DrawIt();
+            LBL_Yellow.Hide();
+            LBL_Green.Hide();
+            LBL_Red.Hide();
+            LBL_White.Hide();
+            LBL_Blue.Hide();
+        }
+        private void BTN_Tourner_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int rInt = r.Next(0, 50); //for ints
+
+            for (int i = 0; i < rInt; i++)
+            {
+                int j = r.Next(0, 5);
+                if (j == 0)
+                {
+                    HideAllCats();
+                    LBL_Green.Show();
+                }
+                if (j == 1)
+                {
+                    HideAllCats();
+                    LBL_Red.Show();
+                }
+                if (j == 2)
+                {
+                    HideAllCats();
+                    LBL_White.Show();
+                }
+                if (j == 3)
+                {
+                    HideAllCats();
+                    LBL_Blue.Show();
+                }
+                if (j == 4)
+                {
+                    HideAllCats();
+                    LBL_Yellow.Show();
+                }
+
+
+            }
         }
     }
 }
